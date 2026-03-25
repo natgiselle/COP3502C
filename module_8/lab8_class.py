3659ac86239448f5c8ebd88b53452b40be26d743
natalie <natgiselle29@gmail.com>
2026-03-25 10:09:10 -0400

    creates OOP Lab Notes

---
+++ module_8/lab8_class.py
@@ -0,0 +1,24 @@
+class People:
+    def __init__(self, age, name): # constructor 
+        self.age = age
+        self.name = name
+
+# self sometimes not used in special caes but almost alwsyd expcet to use it
+# p1 > p2 l(p1,p2)
+
+
+    def say_hi(self):
+        print(f"my name is {self.name}")
+    
+    def get_older(self):
+        age+= 10
+
+p = People(19, 'natbat')
+p2 = People(32, 'peeps')
+
+print(p.age, p2.age)
+p.get_older() #does not take in paramaters if its just self
+print(p.age,p2.age, People.age)
+
+
+
